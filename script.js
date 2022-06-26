57c24432b7832f74b3461ed6276e797be6be241e
brandontaylor156 <brandon.david4@outlook.com>
2022-06-25 20:42:39 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var likeTracker = ['#neil', '#nichole', '#jim'];
+
+function addLike(element) {
+    let likeTrackerIndex = 0;
+
+    if (element.id == 'nicholeButton') {
+        likeTrackerIndex = 1;
+    }
+    else if (element.id == 'jimButton') {
+        likeTrackerIndex = 2;
+    }
+
+    let numLikes = document.querySelector(likeTracker[likeTrackerIndex]).innerHTML;
+    numLikes++;
+    document.querySelector(likeTracker[likeTrackerIndex]).innerHTML = numLikes;
+}
+
+function over(element) {
+    element.style.backgroundColor = "lime";
+}
+
+function out(element) {
+    element.style.backgroundColor = "#ffd966";
+}
