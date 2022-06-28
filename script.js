91707a15921f23f96cf9eb562960748b8fa6e48e
brandontaylor156 <brandon.david4@outlook.com>
2022-06-28 12:04:38 -0700

    changed onclick function

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-var likeTracker = ['#neil', '#nichole', '#jim'];
-
 function addLike(element) {
-    let likeTrackerIndex = 0;
-
-    if (element.id == 'nicholeButton') {
-        likeTrackerIndex = 1;
-    }
-    else if (element.id == 'jimButton') {
-        likeTrackerIndex = 2;
-    }
-
-    let numLikes = document.querySelector(likeTracker[likeTrackerIndex]).innerText;
-    numLikes++;
-    document.querySelector(likeTracker[likeTrackerIndex]).innerText = numLikes;
+    element.innerText++;
 }
 
 function over(element) {
